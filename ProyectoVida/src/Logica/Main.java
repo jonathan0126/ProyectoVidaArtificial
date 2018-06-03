cc4a4394409d50c6fb1d3d59270badae16c46a7a
jonathan0126 <39790533+jonathan0126@users.noreply.github.com>
2018-06-03 13:23:08 -0500

    test 2
    test 2

--- ProyectoVida/src/Logica/Main.java
+++ ProyectoVida/src/Logica/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
             FrameSimulacion simulacion= new FrameSimulacion();
             simulacion.setVisible(true);
+            System.out.println("Propbando...");
 	}
 
 }
